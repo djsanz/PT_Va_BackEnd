4316eda50479b15d6418cd06d7a899f3f27e1f82
djsanz@gmail.com <djsanz@gmail.com>
2024-01-04 21:32:38 +0100

    Quitar Limite Rate

--- api/LoginApi.js
+++ api/LoginApi.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const LoginController = require('../Controllers/LoginController')
-const RateLimiter = require('../Middleware/RateLimitMiddleware')
 
-router.use(RateLimiter(1000, 4))
 router.post('/', LoginController.Login)
 
 module.exports = router
--- api/RootApi.js
+++ api/RootApi.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const RootController = require('../Controllers/RootController')
-const RateLimiter = require('../Middleware/RateLimitMiddleware')
 
-router.use(RateLimiter(1000, 4))
 router.get('/', RootController.Root) // Muestra la página de inicio
 router.get('/ping', RootController.Ping) // devuelve pong para comprobar que el servidor está activo
 router.get('/favicon.ico', RootController.FavIcon) // devuelve el favicon.ico
